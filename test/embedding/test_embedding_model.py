87db46ca2dde9635ffd9827e0de174a1cb92e949
杰洛特的猫 <876539460@qq.com>
2025-09-03 01:42:43 +0800

    增加embedding

---
+++ test/embedding/test_embedding_model.py
@@ -0,0 +1,29 @@
+from src.libs.log import logger
+from src.libs.record_time import record_time
+
+BGE_LARGE_ZH_V1_5 = r"E:\huggingface_cache\hub\models--BAAI--bge-large-zh-v1.5\bge-large-zh-v1.5"
+
+@record_time
+def test_sentence_to_embedding():
+    from sentence_transformers import SentenceTransformer
+
+    # 加载模型（第一次会自动下载到本地缓存）
+    model = SentenceTransformer(BGE_LARGE_ZH_V1_5)
+
+    sentences = ["人工智能正在改变世界", "我喜欢机器学习"]
+    embeddings = model.encode(sentences, normalize_embeddings=True)  # 建议归一化
+
+    logger.info(f"Embedding 向量维度: {embeddings.shape}")
+    logger.info(f"第一个句子的向量前10维: {embeddings[0][:10]}")
+
+
+def test_similarity():
+    from sentence_transformers import SentenceTransformer, util
+
+    model = SentenceTransformer(BGE_LARGE_ZH_V1_5)
+
+    sentences = ["人工智能正在改变世界", "我喜欢机器学习"]
+    embeddings = model.encode(sentences, normalize_embeddings=True)
+
+    cos_sim = util.cos_sim(embeddings[0], embeddings[1])
+    logger.info("句子相似度:", cos_sim.item())
